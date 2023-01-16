7db7afb9723c2712a93ca290016912257e99e480
Halasz Istvan <ihalasz@pentalog.com>
2023-01-16 18:09:54 +0200

    Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,12 @@
-const HomePage = () => <h1>My Home Page</h1>;
+import { Link } from 'react-router-dom';
+
+const HomePage = () => (
+  <>
+    <h1>My Home Page</h1>
+    <p>
+      Go to <Link to="/products">the list of products</Link>.
+    </p>
+  </>
+);
 
 export default HomePage;
